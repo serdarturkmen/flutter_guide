d0ce1545ca71780f96313141d65919ad96415b3d
Serdar Türkmen <baver_t@hotmail.com>
2021-11-03 10:43:35 +0300

    statefull app with functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,50 @@ import 'package:flutter/material.dart';
 //   runApp(MyApp());
 // }
 
-void main()=> runApp(MyApp()));
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIdex = 0;
+
+  void answerQuestion(num) {
+    setState(() {
+      questionIdex = num;
+    });
+    print(questionIdex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favaorite color?',
+      'What\'s your favaorite animal?'
+    ];
     return MaterialApp(
-      home: Text('hello1!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Text(questions[questionIdex]),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: () => answerQuestion(0),
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: () => answerQuestion(1),
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: () => answerQuestion(0),
+          )
+        ]),
+      ),
     );
   }
 }
